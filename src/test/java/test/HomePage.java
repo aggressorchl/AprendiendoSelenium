03328c200d14ba6c53975d87bc14a2e62d50e78b
aggressorchl <51488395+aggressorchl@users.noreply.github.com>
2019-09-03 15:06:16 -0400

    Primer commit

--- src/test/java/test/HomePage.java
+++ src/test/java/test/HomePage.java
@@ -8,14 +8,16 @@ import util.configs.BaseConfig;
 public class HomePage extends BaseConfig {
 
     @Test
-    public void testHomePage() throws InterruptedException {
+    public void testHomePage() {
 
         //Catching web elements
         WebElement pageTitle = getWebDriver().findElement(By.className("site-title"));
         WebElement entryContent = getWebDriver().findElement(By.className("entry-content"));
+        WebElement searchBar = getWebDriver().findElement(By.className("search-field"));
 
         //Assertions
         Assert.assertEquals("Automation Test Page",pageTitle.getText());
         Assert.assertEquals("Bienvenido(a) a WordPress. Esta es tu primera entrada. Edítala o bórrala ¡y comienza a publicar!",entryContent.getText());
+        searchBar.sendKeys("UWU");
     }
 }
