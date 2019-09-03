73a50beb268b272f4cc49be9de0711bc4106d1b1
aggressorchl <51488395+aggressorchl@users.noreply.github.com>
2019-09-03 16:01:40 -0400

    Segundo commit

--- src/test/java/test/HomePage.java
+++ src/test/java/test/HomePage.java
@@ -18,6 +18,9 @@ public class HomePage extends BaseConfig {
         //Assertions
         Assert.assertEquals("Automation Test Page",pageTitle.getText());
         Assert.assertEquals("Bienvenido(a) a WordPress. Esta es tu primera entrada. Edítala o bórrala ¡y comienza a publicar!",entryContent.getText());
+
         searchBar.sendKeys("UWU");
+        searchBar.clear();
+        searchBar.sendKeys("ewe");
     }
 }
